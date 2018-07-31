e04c343339984cdd1797adfd392df441d1e69813
rayonhunte <rayon.hunte@gmail.com>
2018-07-31 12:02:05 -0400

    search ahead :)

--- server/get-courses.route.ts
+++ server/get-courses.route.ts
@@ -10,19 +10,19 @@ export function getAllCourses(req: Request, res: Response) {
 
     const error = (Math.random() >= 0.5);
 
-    if (error) {
-        console.log('ERROR loading courses!');
-        res.status(500).json({message: 'random error occurred.'});
-    } else {
+    // if (error) {
+    //     console.log('ERROR loading courses!');
+    //     res.status(500).json({message: 'random error occurred.'});
+    // } else {
 
         setTimeout(() => {
 
-             // res.status(200).json({payload: Object.values(COURSES)});
-             res.status(500).json({message: 'random error occurred.'});
+             res.status(200).json({payload: Object.values(COURSES)});
+             // res.status(500).json({message: 'random error occurred.'});
 
         }, 200);
 
-    }
+   // }
 }
 
 
--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -46,15 +46,13 @@ export class CourseComponent implements OnInit, AfterViewInit {
     }
 
     ngAfterViewInit() {
-        const searchLessons$ = fromEvent<any>(this.input.nativeElement, 'keyup').pipe(
+        this.lessons$ = fromEvent<any>(this.input.nativeElement, 'keyup').pipe(
             map(event => event.target.value),
+            startWith(''),
             debounceTime(200),
             distinctUntilChanged(),
             switchMap(search => this.loadLessons(search))
         );
-
-        const initialLessons$ = this.loadLessons();
-        this.lessons$ = concat(initialLessons$, searchLessons$);
     }
 
     loadLessons(search = ''): Observable<Lesson[]> {
